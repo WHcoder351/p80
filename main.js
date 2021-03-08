69e3e473bc9da66f380ffc7967456d85cfda3235
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-03-08 07:31:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-var img = ["https://media.istockphoto.com/vectors/happy-fathers-day-man-open-shirt-to-show-super-dad-vector-id931805078?k=6&m=931805078&s=612x612&w=0&h=Ec0VoyAs9SlYp-xXTd9VPDEaJ-tULt-BLqpXyQmqDME=",
-  "https://static.vecteezy.com/system/resources/previews/000/126/700/original/super-mom-vector.jpg",
-  "https://image.shutterstock.com/z/stock-vector-big-brother-reading-to-younger-brother-vector-22804474.jpg"];
-  var names = ["Rani","Raj","Jason&Jadon"]
+var img = ["https://clipartstation.com/wp-content/uploads/2018/10/working-father-clipart-4.jpg",
+  "https://webstockreview.net/images/mom-clipart-cartoon-4.png",
+  "https://th.bing.com/th/id/R277a8e105c31d3209f29cd2ebdebe261?rik=QQHkFbfBrjyfsA&riu=http%3a%2f%2fclipart-library.com%2fnewimages%2fboy-clipart-14.jpg&ehk=8MZ8N5ALHqP2zWJvCywpCLMXx9m6JC9gfxd8k0eHurU%3d&risl=&pid=ImgRaw",
+  "https://s-media-cache-ak0.pinimg.com/564x/47/e8/15/47e815e8fa0e4a00f1fe8dc017f48ccb.jpg"];
+  var names = ["Raja","Rani","Jason","Jadon"]
 var i = 1;
 function move() {
   document.getElementById("album1").src = img[i]; 
-  document.getElementById("name").src = names[i]; 
+  document.getElementById("name").innerHTML = names[i]; 
 i++;
-if (i==3)
+if (i==4)
   {
     i=0; }
 }
